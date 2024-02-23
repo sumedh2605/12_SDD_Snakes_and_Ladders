6dcebfed5756357e92e0112be4a1b5fbdf6a0023
ProSkilledCrow <sufiyan.shaik006@gmail.com>
2024-02-23 20:29:15 +1100

    proper upload
    adding a function and adding a callback file while also changing varaiible names to allow for a better integration experience

--- snake_and_ladder_check/snake_and_ladder_check.py
+++ snake_and_ladder_check/snake_and_ladder_check.py
@@ -1,3 +1,4 @@
+projected_new_position = int(input("Enter an integer: "))
 
 ladders = {71: 92,
            61: 82,
@@ -11,21 +12,25 @@ snakes = {97: 78,
           88: 38,
           62: 18,
           32: 10,
-          36: 5}
+          35: 5}
 
 
-user_input = int(input("Enter an integer: "))
+def snake_and_ladder_check():
 
+    # Check if the input matches any ladder key
+    if projected_new_position in ladders:
+        print(f"Ladder found! Climb from {projected_new_position} to {ladders[projected_new_position]}.")
+        confirmed_new_position = int(ladders[projected_new_position])  #the value that should be shared with other modules
 
-# Check if the input matches any ladder key
-if user_input in ladders:
-    print(f"Ladder found! Climb from {user_input} to {ladders[user_input]}.")
-    endpoint_ladder = ladders[user_input] #the value that should be shared with other modules
+    # Check if the input matches any snakes key
+    elif projected_new_position in snakes:
+        print(f"Snake found! Slide down from {projected_new_position} to {snakes[projected_new_position]}.")
+        confirmed_new_position = int(snakes[projected_new_position]) #the value that should be shared with other modules
 
-# Check if the input matches any snakes key
-elif user_input in snakes:
-    print(f"Snake found! Slide down from {user_input} to {snakes[user_input]}.")
-    endpoint_snake = snakes[user_input] #the value that should be shared with other modules
-# If no match is found
-else:
-    print("No ladder or snake found for the given input.")
+    # If no match is found
+    else:
+        print("No ladder or snake found for the given input.")
+
+    print(confirmed_new_position)
+
+snake_and_ladder_check()
