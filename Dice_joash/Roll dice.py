4d82bc691d5d25a481d9407e73c4784a715f6e8e
JoashSH <160975319+JoashSH@users.noreply.github.com>
2024-02-29 11:29:06 +1100

    Made dice roll function in Roll dice.py
    Turned it into a function

--- Dice_joash/Roll dice.py
+++ Dice_joash/Roll dice.py
@@ -1,12 +1,14 @@
 import random
-number = (1, 2)
-rollednumber = []
-for i in range(2):
-    rollednumber.append(random.randint(1, 6))
-print (rollednumber)
 
-a = rollednumber[0]+ rollednumber[1]
-if a == 12 :
-    print ("you get a second chance")
-else:
-    print("you do not get a second chance")
+def dice_roll():
+    number = (1, 2)
+    rollednumber = []
+    for i in range(2):
+        rollednumber.append(random.randint(1, 6))
+    return(rollednumber)
+
+    a = rollednumber[0]+ rollednumber[1]
+    if a == 12 :
+        print ("you get a second chance")
+    else:
+        print("you do not get a second chance")
