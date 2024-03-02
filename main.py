5d7ca47a927a7ee727dbccdd362ab7c62ff54ef6
crookedqq <160854729+crookedqq@users.noreply.github.com>
2024-03-02 13:06:36 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -2,76 +2,102 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((700, 700)) #changed
 
 grid = []
-for i in range (750, 0, -75):#y coordinate
+for i in range (649, -51, -70):#y coordinate changed
     row = []
-    for j in range (0, 750, 75):#x coordinates
+    for j in range (19, 719, 70):#x coordinates changed
         row.append((j,i))
     grid.append(row)
 
+ladders = {71: 92,
+           61: 82,
+           56: 75,
+           36: 77,
+           9: 30,
+           6: 17}
+
+snakes = {97: 78,
+          95: 56,
+          88: 38,
+          62: 18,
+          32: 10,
+          35: 5}
+
 # stubs
-projected_positon = 8
-confirmed_position = 13
-dice_sum = 8
+projected_positon = 6
+confirmed_position = 17
+dice_sum = 13
 ladderstartX, ladderstartY = grid[0][8]
 ladderendX, ladderendY = grid[4][7]
 snakestartX, snakestartY = grid[3][4]
 snakeendX, snakeendY = grid[1][2]
-turn = 1 #whose turn it is
+turn = 0 #whose turn it is
 
 
 
 #converts board number to dictionary coord
-Y = int(confirmed_position/10)
-X = int(confirmed_position%10)
+
 
 # loads all images and starting coordinates
 pawn1 = pygame.image.load(r'images/pawn.png')
-Xone, Yone = grid[0][0]
+pawn1 = pygame.transform.scale(pawn1, (32,32))
+Xg1, Yg1 = 0, 0
+Xone, Yone = grid[Xg1][Yg1]
 pawn2 = pygame.image.load(r'images/pawn2.png')
-Xtwo, Ytwo = grid[0][0]
+pawn2 = pygame.transform.scale(pawn2, (32,32))
+Xg2, Yg2 = 0, 0
+Xtwo, Ytwo = grid[Xg2][Yg2]
 pawn3 = pygame.image.load(r'images/pawn3.png')
-Xthree, Ythree = grid[0][0]
+pawn3 = pygame.transform.scale(pawn3, (32,32))
+Xg3, Yg3 = 0, 0
+Xthree, Ythree = grid[Xg3][Yg3]
 pawn4 = pygame.image.load(r'images/pawn4.png')
-Xfour, Yfour = grid[0][0]
+pawn4 = pygame.transform.scale(pawn3, (32,32))
+Xg4, Yg4 = 0, 0
+Xfour, Yfour = grid[Xg4][Yg4]
 
 #lists for convinience
 pawns = [pawn1, pawn2, pawn3, pawn4]
-pawn_names = ['1','2','3','4']
 coords = [[Xone, Yone],[Xtwo, Ytwo], [Xthree, Ythree], [Xfour,Yfour]]
+gridcoord = [[Xg1, Yg1],[Xg2,Yg2],[Xg3,Yg3],[Xg4,Yg4]]
 
-r=0
 
 resized = [False, False, False, False]
+X_board = 0
 
-def movement(X, Y, d):
-    if d < dice_sum:
+def movement(X, Y, X_board):
+    if position < projected_positon:
         pygame.display.update()
-        if Y == 675 or Y == 525 or Y == 375 or Y == 225 or Y == 75:  # switch direction every other row
-            X -= 75
+        if Y == 1 or Y == 3 or Y == 5 or Y == 7 or Y == 9:  # switch direction every other row
+            X -= 1
+            if X < 0:
+                X = 0
+                Y -= 1
+            X_board = (10-X)
+            print (X)
+            print (X_board)
         else:
-            X += 75
-
-        if X > 675:  # border to go up one row
-            X = 675
-            Y -= 75
-        if X < 0:
-            X = 0
-            Y -= 75
-        coords[turn][0] = X
-        coords[turn][1] = Y
-    return(X,Y)
+            X += 1
+            X_board = X
+            if X > 9:  # border to go up one row
+                X = 9
+                X_board = (10-X)
+                Y += 1
+        gridcoord[turn][0], gridcoord[turn][1] = X, Y
+    return(X,Y, X_board)
 
 def snakeorladders(Xpos, Ypos):
-    Xdif = (ladderendX - Xpos)
-    Ydif = (ladderendY -Ypos)
-    Xpos += Xdif
-    Ypos += Ydif
+    if position in ladders:
+        new_position = ladders[position]
+    elif position in snakes:
+        new_position = snakes[position]
+    Ypos = int(new_position / 10)
+    Xpos = int(new_position % 10)
     coords[turn][0] = Xpos
     coords[turn][1] = Ypos
-    return (Xpos, Ypos)
+    return Xpos, Ypos
 
 
 #Game loop
@@ -86,21 +112,19 @@ while running:
     screen.blit(pawn3, (coords[2][0], coords[2][1]))
     screen.blit(pawn4, (coords[3][0], coords[3][1]))
 
-    Xpos = coords[turn][0]
-    Ypos = coords[turn][1]
+    Xpos = gridcoord[turn][0]
+    Ypos = gridcoord[turn][1]
 
+    position = Ypos * 10 + X_board
 
     # call function to move correct player across the board
-    Xpos, Ypos = movement(Xpos , Ypos, r)
-    if r != dice_sum:
-        r += 1 # increment the number of steps move
+    Xpos, Ypos, X_board = movement(Xpos, Ypos, X_board)
+    coords[turn][0], coords[turn][1] = grid[Ypos][Xpos]
 
 
-    # call function to move player on a ladder
-    if (Xpos, Ypos) == (ladderstartX, ladderstartY) and r == dice_sum:
+    if position in ladders and position == projected_positon or position in snakes and position == projected_positon:
         Xpos, Ypos = snakeorladders(Xpos, Ypos)
-    if (Xpos, Ypos) == (snakestartX, snakestartY) and r == dice_sum:
-        Xpos, Ypos = snakeorladders(Xpos, Ypos)
+        coords[turn][0], coords[turn][1] = grid[Ypos][Xpos]
     pygame.display.update()
 
 
